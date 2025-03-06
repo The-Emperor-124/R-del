309debf8b968a23b83a10a409a0b9e3b62077be6
Radu0509 <raducraiasu@yahoo.com>
2025-03-06 22:39:47 +0200

    Add files via upload

---
+++ Ex2/src/class Carte {.java
@@ -0,0 +1,13 @@
+class Carte 
+{
+ private int NrPag;
+    
+    
+    public Carte(int NrPag){ this.NrPag = NrPag}
+    
+    
+
+    public int getNrPag(){return NrPag;}
+       
+       
+    }
---
+++ Ex3/src/Main.Java.java
@@ -0,0 +1,14 @@
+ public class Main  
+{
+    public static void main(String[] args){
+   Patrat A1=new Patrat();
+   Patrat A2=new Patrat(15);
+   Patrat A3=new Patrat(19);
+   A1.afiseaza;
+   A2.afiseaza;
+   A3.afiseaza;
+    }
+
+
+
+}
